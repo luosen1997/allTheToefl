7ac14ddc32ed33e51e106ddddf867e3e820b50dd
yyt123yyt <571121866@qq.com>
2021-08-12 11:14:30 +0800

    上传

---
+++ src/main/java/com/project812/tuofu/TuofuApplication.java
@@ -0,0 +1,13 @@
+package com.project812.tuofu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuofuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuofuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project812/tuofu/TuofuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project812.tuofu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuofuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
